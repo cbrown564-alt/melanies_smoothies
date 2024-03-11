776fe3730b36187af4e9b0088c17498f90329beb
cbrown564-alt <121303348+cbrown564-alt@users.noreply.github.com>
2024-03-11 15:29:19 +0000

    Update streamlit_app.py
    fruit nutrition information subhead

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
     my_dataframe,
-    max_selections=5
+    max_selections = 5
 )
 
 if ingredients_list:
@@ -31,8 +31,9 @@ if ingredients_list:
     ingredients_string=''
     
     for fruit_chosen in ingredients_list:
-        ingredients_string+=fruit_chosen +' '
-        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=TRUE)
 
     #st.write(ingredients_string)
