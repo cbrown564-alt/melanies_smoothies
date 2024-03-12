d7cd82f0abd3755b7e3ae827c5f6a8c96020c9e4
cbrown564-alt <121303348+cbrown564-alt@users.noreply.github.com>
2024-03-12 10:22:42 +0000

    Update streamlit_app.py
    flow through to nutrition information, remove stop at fruit_name and search_on dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark DataFrame to a Pandas Datafram so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
@@ -44,7 +44,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=TRUE)
 
     #st.write(ingredients_string)
